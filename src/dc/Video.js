dea7c936e5114213a91043fa3aeed8b657d5eddd
cerelus <cerelus3@gmail.com>
2023-06-13 11:03:25 +0900

    0

---
+++ src/dc/Video.js
@@ -0,0 +1,15 @@
+// DC 메인 페이지 컴포넌트
+import React from "react";
+import isrc from "./ImgSrc";
+import VidIntro from "./modules/VidIntro";
+
+const Video = () => {
+    return(
+        <>
+            <h2>Video 페이지</h2>
+            <VidIntro pg="VIDEO" mm="on" />
+        </>
+    );
+}; /////////// Video //////////////
+
+export default Video;
